0e1c751dbd330d06b796e95b659ac842dc94edb4
frc857 <frc857.tmp351c3@mail.goobisoft.com>
2022-02-17 20:44:33 -0500

    Update example code to what motors are actually on our robot

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,23 @@
+package frc.robot.subsystems;
+
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.Constants.DriveConstants;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class IntakeSubsystem {
+    VictorSP m_intakeMotor = new VictorSP(IntakeConstants.kIntakeMotorPort);
+    DoubleSolenoid m_intakeSolenoid = new Doublesolenoid(IntakeContstants); // ??????
+}
