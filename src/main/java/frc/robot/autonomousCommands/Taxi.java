54520992d57f5e5b89c72af6f2479fdf0e46af82
frc857 <frc857.tmp351c3@mail.goobisoft.com>
2022-03-03 18:45:07 -0500

    Decrease drive distance a bit to avoid side walls of field

--- src/main/java/frc/robot/autonomousCommands/Taxi.java
+++ src/main/java/frc/robot/autonomousCommands/Taxi.java
@@ -57,7 +57,7 @@ public class Taxi {
             // Start at the origin facing the +X direction
             new Pose2d(0, 0, new Rotation2d(0)),
             // End 3 meters straight ahead of where we started, facing forward
-            new Pose2d(3, 0, new Rotation2d(0))),
+            new Pose2d(2, 0, new Rotation2d(0))),
             // Pass config
             config);
 
