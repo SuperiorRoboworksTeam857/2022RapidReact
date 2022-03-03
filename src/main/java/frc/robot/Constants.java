7f0f4238d712f9b3fafe2c320a46850a57ae1e0b
frc857 <frc857.tmp351c3@mail.goobisoft.com>
2022-03-03 18:38:21 -0500

    Flip front of robot to intake side, fix solenoid ports, slow down auto

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,10 +16,10 @@ import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
  */
 public final class Constants {
   public static final class DriveConstants {
-    public static final int kLeftMotor1Port = 23;
-    public static final int kLeftMotor2Port = 24;
-    public static final int kRightMotor1Port = 20;
-    public static final int kRightMotor2Port = 21;
+    public static final int kLeftMotor1Port = 20;
+    public static final int kLeftMotor2Port = 21;
+    public static final int kRightMotor1Port = 23;
+    public static final int kRightMotor2Port = 24;
 
     public static final double kTrackwidthMeters = 0.5797;
     public static final DifferentialDriveKinematics kDriveKinematics =
@@ -46,36 +46,43 @@ public final class Constants {
 
   public static final class IntakeConstants {
     public static final int kIntakeMotorPort = 0;
-    public static final int kIntakeForwardPort = 7;
-    public static final int kIntakeReversePort = 15;
+    public static final int kIntakeForwardPort = 6;
+    public static final int kIntakeReversePort = 14;
   }
 
   public static final class ClimberConstants {
     public static final int kLeftMotorPort = 25;
     public static final int kRightMotorPort = 22;
-    public static final int kClimberForwardPort = 6;
-    public static final int kClimberReversePort = 14;
+    public static final int kClimberForwardPort = 7;
+    public static final int kClimberReversePort = 15;
   }
 
   public static final class ShooterConstants {
     public static final int kFrontMotorPort = 30;
     public static final int kBackMotorPort = 31;
-    public static final int kKickerForwardPort = 6;
-    public static final int kKickerReversePort = 14;
+    public static final int kKickerForwardPort = 5;
+    public static final int kKickerReversePort = 13;
   }
 
   public static final class OIConstants {
     public static final int kDriverControllerPort = 0;
     public static final int kDriverStickPort = 1;
+    public static final int kButtonBoxPort = 2;
   }
 
   public static final class AutoConstants {
-    public static final double kMaxSpeedMetersPerSecond = 1.5;
+    public static final double kMaxSpeedMetersPerSecond = 0.5; //1.5
     public static final double kFastMaxSpeedMetersPerSecond = 3.5;
     public static final double kMaxAccelerationMetersPerSecondSquared = 2;
 
     // Reasonable baseline values for a RAMSETE follower in units of meters and seconds
     public static final double kRamseteB = 2;
     public static final double kRamseteZeta = 0.7;
+  }
+  public static final class LimelightConstants {
+    // FIGURE THESE OUT
+    public static final double kLimelightHeight = 41.0; // Inches
+    public static final double kLimelightAngle = -91.0; // Degrees 
+    public static final double kTargetHeight = 98.25;
   }
 }
