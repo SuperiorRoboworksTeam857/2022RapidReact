deffe6ed50ea38093bf2a6e3cd52a34aa1c9e367
frc857 <frc857.tmp351c3@mail.goobisoft.com>
2022-03-15 18:31:17 -0400

    Added shooter speed check and added beam break checks

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
@@ -17,46 +18,75 @@ public class ShooterSubsystem extends SubsystemBase{
      ShooterConstants.kKickerForwardPort,
       ShooterConstants.kKickerReversePort);
 
-    public ShooterSubsystem() {
-    
-    }
-  
-    @Override
-    public void periodic() {
-      
-    }
-  
-    private void runShooter(double speed) {
-      frontMotor.set(speed);
-      backMotor.set(speed);
-      SmartDashboard.putNumber("frontShooterRPM", frontMotor.getSensorCollection().getIntegratedSensorVelocity() * 10/2048*60);
-      SmartDashboard.putNumber("backShooterRPM", backMotor.getSensorCollection().getIntegratedSensorVelocity() * 10/2048*60);
-    }
+  public enum Height {NotShooting, ShootingHigh, ShootingLow};
+  Height m_height;
 
-    public void stopShooter() {
-      runShooter(0);
-    }
+  DigitalInput beamBreak = new DigitalInput(4);
 
-    public void runShooterUpperHub() {
-      runShooter(-0.35);
-    }
+  public ShooterSubsystem() {
+  }
 
-    public void runShooterLowerHub() {
-      runShooter(-.23);
-    }
-  
-  
-    public void lowerKicker() {
-      m_kickerSolenoid.set(Value.kReverse);
-    }
-  
-    public void raiseKicker() {
+  @Override
+  public void periodic() {
+    SmartDashboard.putBoolean("beam", beamBreak.get());
+  }
+
+  private void runShooter(double speed) {
+    frontMotor.set(speed);
+    backMotor.set(speed);
+    SmartDashboard.putNumber("frontShooterRPM", frontMotor.getSensorCollection().getIntegratedSensorVelocity() * 10/2048*60);
+    SmartDashboard.putNumber("backShooterRPM", backMotor.getSensorCollection().getIntegratedSensorVelocity() * 10/2048*60);
+  }
+
+  public void stopShooter() {
+    runShooter(0);
+    m_height = Height.NotShooting;
+  }
+
+  public void runShooterUpperHub() {
+    runShooter(-0.35);
+    m_height = Height.ShootingHigh;
+  }
+
+  public void runShooterLowerHub() {
+    runShooter(-.23);
+    m_height = Height.ShootingLow;
+  }
+
+  public boolean isBallPresent() {
+    return beamBreak.get();
+  }
+
+  public void lowerKicker() {
+    m_kickerSolenoid.set(Value.kReverse);
+  }
+
+  public void raiseKicker() {
+    boolean overrideBeamBreak = false;
+    raiseKicker(overrideBeamBreak);
+  }
+  public void raiseKicker(boolean overrideBeamBreak) {
+    if (isBallPresent() || overrideBeamBreak) {
       m_kickerSolenoid.set(Value.kForward);
     }
-    public boolean isShooterAtSpeedUpperHub() {
-      return frontMotor.getSensorCollection().getIntegratedSensorVelocity() * 10/2048*60 <= -2000;
+  }
+
+  public boolean isShooterAtSpeed() {
+    switch (m_height) {
+      case ShootingLow:
+        return isShooterAtSpeedLowerHub();
+      case ShootingHigh:
+        return isShooterAtSpeedUpperHub();
+      case NotShooting:
+        return false;
     }
-    public boolean isShooterAtSpeedLowerHub() {
-      return frontMotor.getSensorCollection().getIntegratedSensorVelocity() * 10/2048*60 <= -1000; //<----double check
-    }
+    return false;
+  }
+
+  public boolean isShooterAtSpeedUpperHub() {
+    return frontMotor.getSensorCollection().getIntegratedSensorVelocity() * 10/2048*60 <= -2000;
+  }
+  public boolean isShooterAtSpeedLowerHub() {
+    return frontMotor.getSensorCollection().getIntegratedSensorVelocity() * 10/2048*60 <= -1000; //<----double check
+  }
 }
