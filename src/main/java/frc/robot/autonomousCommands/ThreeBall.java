b393ba5513caf55da348b2996d13ec7f19cab8b7
frc857 <frc857.tmp351c3@mail.goobisoft.com>
2022-03-15 18:26:44 -0400

    Added the rest of the path

--- src/main/java/frc/robot/autonomousCommands/ThreeBall.java
+++ src/main/java/frc/robot/autonomousCommands/ThreeBall.java
@@ -18,6 +18,7 @@ import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 import java.util.List;
 
@@ -43,7 +44,7 @@ public class ThreeBall {
             10);
 
     // Create config for trajectory
-    TrajectoryConfig config =
+    TrajectoryConfig firstConfig =
         new TrajectoryConfig(
                 AutoConstants.kMaxSpeedMetersPerSecond,
                 AutoConstants.kMaxAccelerationMetersPerSecondSquared)
@@ -51,8 +52,17 @@ public class ThreeBall {
             .setKinematics(DriveConstants.kDriveKinematics)
             // Apply the voltage constraint
             .addConstraint(autoVoltageConstraint);
+    TrajectoryConfig secondConfig =
+        new TrajectoryConfig(
+                AutoConstants.kMaxSpeedMetersPerSecond,
+                AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+            // Add kinematics to ensure max speed is actually obeyed
+            .setKinematics(DriveConstants.kDriveKinematics)
+            // Apply the voltage constraint
+            .addConstraint(autoVoltageConstraint)
+            .setReversed(true);
 
-    Trajectory taxiTrajectory =
+    Trajectory firstTrajectory =
         TrajectoryGenerator.generateTrajectory(
             List.of(
             new Pose2d(0, 0, new Rotation2d(Math.PI/3)),
@@ -60,11 +70,36 @@ public class ThreeBall {
             new Pose2d(3.8, 1, new Rotation2d(0)),
             new Pose2d(5.0, 1.5, new Rotation2d(Math.PI/3))
             ),
-            config);
+            firstConfig);
+    Trajectory secondTrajectory =
+        TrajectoryGenerator.generateTrajectory(
+            List.of(
+            new Pose2d(5.0, 1.5, new Rotation2d(Math.PI/3)),
+            new Pose2d(3.8, 1, new Rotation2d(0)),
+            new Pose2d(1.5, 1, new Rotation2d(0)),
+            new Pose2d(0, 0, new Rotation2d(Math.PI/3))
+            ),
+            secondConfig);
 
-    RamseteCommand ramseteCommand =
+    RamseteCommand firstRamseteCommand =
         new RamseteCommand(
-            taxiTrajectory,
+            firstTrajectory,
+            robot.m_robotDrive::getPose,
+            new RamseteController(AutoConstants.kRamseteB, AutoConstants.kRamseteZeta),
+            new SimpleMotorFeedforward(
+                DriveConstants.ksVolts,
+                DriveConstants.kvVoltSecondsPerMeter,
+                DriveConstants.kaVoltSecondsSquaredPerMeter),
+            DriveConstants.kDriveKinematics,
+            robot.m_robotDrive::getWheelSpeeds,
+            new PIDController(DriveConstants.kPDriveVel, 0, 0),
+            new PIDController(DriveConstants.kPDriveVel, 0, 0),
+            // RamseteCommand passes volts to the callback
+            robot.m_robotDrive::tankDriveVolts,
+            robot.m_robotDrive);
+    RamseteCommand secondRamseteCommand =
+        new RamseteCommand(
+            secondTrajectory,
             robot.m_robotDrive::getPose,
             new RamseteController(AutoConstants.kRamseteB, AutoConstants.kRamseteZeta),
             new SimpleMotorFeedforward(
@@ -80,16 +115,62 @@ public class ThreeBall {
             robot.m_robotDrive);
 
     // Reset odometry to the starting pose of the trajectory.
-    robot.m_robotDrive.resetOdometry(taxiTrajectory.getInitialPose());
+    robot.m_robotDrive.resetOdometry(firstTrajectory.getInitialPose());
 
     return new SequentialCommandGroup(
         new InstantCommand(() -> robot.m_robotIntake.lowerIntake(), robot.m_robotIntake),
+ 
+        // Shoot preload ball
+        new RunCommand(() -> robot.m_robotShooter.runShooterLowerHub(), robot.m_robotShooter).withInterrupt(robot.m_robotShooter::isShooterAtSpeedLowerHub),
+        new InstantCommand(() -> robot.m_robotShooter.raiseKicker(), robot.m_robotShooter ),
+        new ParallelRaceGroup(
+            new RunCommand(() -> robot.m_robotShooter.runShooterLowerHub(), robot.m_robotShooter),
+            new WaitCommand(1)
+        ),
+        new InstantCommand(() -> robot.m_robotShooter.stopShooter(), robot.m_robotShooter),
+        new InstantCommand(() -> robot.m_robotShooter.lowerKicker(), robot.m_robotShooter ),
+
+        // Drive and pick up next 2 balls
         new ParallelRaceGroup(
             new RunCommand(() -> robot.m_robotIntake.runIntake(1), robot.m_robotIntake),
-            ramseteCommand
+            firstRamseteCommand
         ),
+        new ParallelRaceGroup(
+            new RunCommand(() -> robot.m_robotIntake.runIntake(1), robot.m_robotIntake),
+            secondRamseteCommand
+        ),
+
+        // Stop driving and stop intake
         new InstantCommand(() -> robot.m_robotIntake.runIntake(0), robot.m_robotIntake),
-        new InstantCommand(() -> robot.m_robotDrive.tankDriveVolts(0, 0))
+        new InstantCommand(() -> robot.m_robotDrive.tankDriveVolts(0, 0)),
+
+        // Shoot 2nd ball
+        new RunCommand(() -> robot.m_robotShooter.runShooterLowerHub(), robot.m_robotShooter).withInterrupt(robot.m_robotShooter::isShooterAtSpeedLowerHub),
+        new InstantCommand(() -> robot.m_robotShooter.raiseKicker(), robot.m_robotShooter ),
+        new ParallelRaceGroup(
+            new RunCommand(() -> robot.m_robotShooter.runShooterLowerHub(), robot.m_robotShooter),
+            new WaitCommand(1)
+        ),
+        new InstantCommand(() -> robot.m_robotShooter.lowerKicker(), robot.m_robotShooter ),
+
+        // Wait for 3rd ball to roll into shooter
+        new InstantCommand(() -> robot.m_robotIntake.raiseIntake(), robot.m_robotIntake),
+        new WaitCommand(1),
+        new WaitCommand(2).withInterrupt(robot.m_robotShooter::isBallPresent),
+
+        // Shoot 3rd ball
+        new RunCommand(() -> robot.m_robotShooter.runShooterLowerHub(), robot.m_robotShooter).withInterrupt(robot.m_robotShooter::isShooterAtSpeedLowerHub),
+        new InstantCommand(() -> robot.m_robotShooter.raiseKicker(), robot.m_robotShooter ),
+        new ParallelRaceGroup(
+            new RunCommand(() -> robot.m_robotShooter.runShooterLowerHub(), robot.m_robotShooter),
+            new WaitCommand(1)
+        ),
+
+        new InstantCommand(() -> robot.m_robotShooter.lowerKicker(), robot.m_robotShooter ),
+        new InstantCommand(() -> robot.m_robotShooter.stopShooter(), robot.m_robotShooter)
+
     );
+
+
   }
 }
