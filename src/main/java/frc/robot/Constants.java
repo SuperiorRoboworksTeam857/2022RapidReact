ad8b029739cd11a33fd2a8f7ef69efcbd72d2de2
frc857 <frc857.tmp351c3@mail.goobisoft.com>
2022-03-29 20:58:05 -0400

    added motor port for new agitator

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -48,6 +48,7 @@ public final class Constants {
     public static final int kIntakeMotorPort = 0;
     public static final int kIntakeForwardPort = 6;
     public static final int kIntakeReversePort = 14;
+    public static final int kIntakeAgitatorPort = 9;
   }
 
   public static final class ClimberConstants {
